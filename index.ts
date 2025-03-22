9e89c72bc090d0370d696b24598e8956d0ada2ab
Aryan Rajeev <raghavaan27@gmail.com>
2025-03-22 14:10:09 +0530

    Testing basic types in typescript

---
+++ index.ts
@@ -0,0 +1,23 @@
+// TS Practice
+let favNumber: number = 50;
+let greetings: string = 'Hello';
+let bool: boolean = true;
+let numArr: number[] = [];
+let stringArr: string[] = [];
+let mixedArr: (number | string)[] = [];
+
+numArr.push(3);
+numArr.push(6);
+stringArr.push("Hello");
+stringArr.push("World");
+
+// Mixed array: Numbers and strings
+mixedArr.push(42);
+mixedArr.push('Test');
+
+console.log(favNumber);
+console.log(greetings);
+console.log(bool);
+console.log(numArr);
+console.log(stringArr);
+console.log(mixedArr);
